28241086c563cab87bbaec2f35cbbcad22e6c740
AjayKrishna <ajay.akarapu@owitglobal.com>
2019-11-15 15:23:00 +0530

    Simple Setup Spring Cloud

--- EurekaClient/src/main/java/com/springCloud/eurekaClient/EurekaClientApp.java
+++ EurekaClient/src/main/java/com/springCloud/eurekaClient/EurekaClientApp.java
@@ -4,19 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.discovery.EurekaClient;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class EurekaClientApp {
 	
 	@Autowired
     @Lazy
     private EurekaClient eurekaClient;
+
+	@Autowired
+    private RestTemplate restTemplate;
+
+	@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
  
     @Value("${spring.application.name}")
     private String appName;
@@ -28,6 +43,14 @@ public class EurekaClientApp {
     @RequestMapping("/greeting")
     public String greeting() {
         return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
+
+    @RequestMapping("/")
+    public String hello(){
+        String response = restTemplate.exchange("http://emp-service/employees",
+                HttpMethod.GET, null, String.class).getBody();
+
+        return response;
     }
 
 }
--- springCloud/src/main/java/com/eureka/springCloud/App.java
+++ springCloud/src/main/java/com/eureka/springCloud/App.java
@@ -1,13 +1,57 @@
 package com.eureka.springCloud;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<String> l = new ArrayList<String>();
+        String s1 = "ajay";
+        String s2 = "asdsd";
+        String s3 = new String("nnnnn");
+
+//        l.get(0);
+        l.add(s1);
+        l.add(s2);
+        l.add(s3);
+//        System.out.println( "Hello World!" + l.toString() );
+//        s2="wwww";
+//        s3 = new String("aaaa");
+//        System.out.println( "Hello World!" + l.toString() );
+//        List<Employee> l1 = new ArrayList<Employee>();
+//        Employee e1 = new Employee("ajay", 12);
+//        l1.add(e1);
+//        System.out.println( "Hello World!" + l1.toString() );
+//        e1.setName("asdsad");
+//        System.out.println( "Hello World!" + l1.toString() );
+
+        String ss = "aasdsad^{dfdf";
+//        l.stream().filter();
+        ss = ss.replace("^{","{");
+        System.out.println( "Hello World!" + ss);
+
+//        Map map = new HashMap();
+//        StringBuilder sb = new StringBuilder("java");
+//        sb.append(s1);
+//        sb.substring(4);
+//        System.out.println( "Hello World!" + sb);
+//        System.out.println( "Hello World!" + sb.indexOf(s1));
+//
+//        abs a = new App();
+//        a.m1();
+
+        l.get(4);
+
+
+
     }
 }
